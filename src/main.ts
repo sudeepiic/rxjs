a94278ad89202ddaac0b165a7bf937d181d3ae18
sudeepiic <sudeep.kuchara.dev.iic@gmail.com>
2024-01-28 17:36:14 +0530

    tap operator

--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,17 @@
 import "./style.css";
 
-import {
-  fromEvent,
-  filter,
-  map,
-  of,
-  reduce,
-  range,
-  interval,
-  take,
-  scan,
-  from,
-} from "rxjs";
+import { map, range, tap } from "rxjs";
 
 // rxjs
-const observer = {
-  next: (val: any) => console.log(val),
-  error: (val: any) => console.error(val),
-  complete: () => {
-    console.table("completed");
-  },
-};
 
-// streams
-const counter$ = interval(1000);
-const btn = document.querySelector("#counter")!;
-counter$
-  .pipe(
-    map(() => -1),
-    scan((acc, crv) => acc + crv, 6),
-    filter((val) => val >= 0)
-  )
-  .subscribe({
-    next: (val) => {
-      if (val) {
-        btn.innerHTML = val.toString();
-      } else {
-        btn.innerHTML = "Time's up";
-      }
-    },
-    complete() {
-      btn.innerHTML = "Time's up";
-    },
-  });
+const observable = range(0, 5);
 
-console.log(`// streams
-const counter$ = interval(1000);
-const btn = document.querySelector("#counter")!;
-counter$
+// tap: use it to log observable streams
+
+observable
   .pipe(
-    map(() => -1),
-    scan((acc, crv) => acc + crv, 20),
-    filter((val) => val >= 0)
+    tap((x) => console.log("before:", x)),
+    map((x) => x * 2),
+    tap((x) => console.log("after:", x))
   )
-  .subscribe({
-    next: (val) => {
-      btn.innerHTML = val.toString();
-      return val;
-    },
-  });`);
+  .subscribe();
